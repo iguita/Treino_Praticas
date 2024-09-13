d502f1f34bd6db1c7206463909f992535628ec5b
iguita <125404944+iguita@users.noreply.github.com>
2024-09-13 19:25:30 -0300

    Add files via upload

---
+++ P&A/Exe1/Exe1/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+
+        double c, l, A;
+
+        Console.Write("Digite o Comprimento: ");
+        c = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite Largura: ");
+        l = double.Parse(Console.ReadLine());
+
+        A = c + l ;
+
+        Console.Write("A area da Sala é: {0}", A);
+
+
+
+    }
+}
